18555c053ed55c7346c9fcb13d9adde793f2607d
Derron <yra2xx@rambler.ru>
2016-08-16 23:52:41 +0300

    Добавлен режим ЧМ

--- amAsm2/dmodes.asm
+++ amAsm2/dmodes.asm
@@ -1,6 +1,5 @@
-
 ;//*************************************************************************
-;//Генерация АМ. Вместо АЦП используется значение, полученной с ПК в R18
+;//Генерация АМ
 ;//В GENR1 - 0 если источник внешний, 1 если внутренний
 ;//*************************************************************************
 dmode1:
@@ -63,30 +62,66 @@ dloop1:
 rjmp dloop1
 
 
+;//*************************************************************************
+;//Генерация ЧМ
+;//В GENR1 - 0 если источник внешний, 1 если внутренний
+;//*************************************************************************
 dmode2:
-	rcall	clearstack
+	rcall clearstack
 
-	ldi		GENR1,	0b00111111			;Настройка PORTB
-	out		DDRB,	GENR1
+	;Настраиваем регистр START
+	clr		STATR
+	andi	GENR1,		1
+	or		STATR,		GENR1		;Режим инф. сигнала внешний/внутренний
+	ldi		GENR1,		2
+	lsl		GENR1
+	or		STATR,		GENR1		;Номер режима
 
-	ldi		GENR2,	1<<WGM10
+	ldi		ZL,	LOW(FM_GEN)
+	ldi		ZH,	HIGH(FM_GEN)
+	rcall   setTimer1COMPAVect
+
+	;Настройка таймеров
+	ldi		GENR2,	0b0000000
+	out		TCCR0,	GENR2
+
+	ldi		GENR2,	200
+	out		OCR1AL,	GENR2
+
+	ldi		GENR2,	(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<FOC1A) | (0<<FOC1B) | (0<<WGM11) | (0<<WGM10)
 	out		TCCR1A,	GENR2
 
-	ldi		GENR2,	(1<<WGM12) | (0<<CS12) | (0<<CS11) | (1<<CS10) 
+	ldi		GENR2,	(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (1<<WGM12) | (0<<CS12) | (0<<CS11) | (1<<CS10) 
 	out		TCCR1B,	GENR2
 
-	ldi		GENR2, (1<<OCIE1B) | (1<<TOIE1)
+	ldi		GENR2, (1<<OCIE1A)
 	out		TIMSK,	GENR2
+	sei;
 
-	sei
+	sbrs	STATR,		0		;Если первый бит START - 1, то  запускаем таймер для генерации внутр. сигнала
+	rjmp	dloop1
+	
+	ldi		GENR1,		100
+	mov		SIGAR,		GENR1
+	
+	ldi		GENR1,		(1<<WGM21) | (1<<CS21) | (1<<CS20)
+	out		TCCR2,		GENR1
+
+	ldi		GENR1,		100
+	out		OCR2,		GENR1
+	in		GENR1,		TIMSK
+	ori		GENR1,		(1<<OCIE2)
+	out		TIMSK,		GENR1
+
 dloop2:
-	ldi		GENR1,		(1<<REFS0) | (1<<ADLAR) | (1<<MUX2) | (1<<MUX0)	
+
+	sbrc	STATR,		0
+	rjmp	dloop2
+	
+	;Если сигнал внешний, то считываем его значение с АЦП
+	ldi		GENR1,		(1<<REFS0) | (1<<ADLAR) | (1<<MUX2) | (1<<MUX0)
 	rcall	getADCSample
-	in		MSIGR,		ADCH
+	in		MSIGR,		ADCH	
 
-rjmp dloop2
-
-
-
-
+rjmp dloop1
 
